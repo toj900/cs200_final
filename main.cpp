532cf606730fa010204a332c38af9dfae3fb25cb
toj900 <46387607+toj900@users.noreply.github.com>
2020-12-03 16:47:09 -0500

    Print Hangman
    Added hangman diagram to scoreboard

--- main.cpp
+++ main.cpp
@@ -38,9 +38,12 @@ class History: public Word{
 };
 class Player{
 private:
-    int lives = 6;
+    int lives;
 
 public:
+    Player(){
+        lives =6;
+    }
     void setLives(int reduction){
         lives -= reduction;
     }
@@ -50,16 +53,79 @@ public:
 };
 
 class ScoreBoard{
+private:
+    std::string hangman[7];
+public:
+    void setPositions(std::string *positions, int size){
+        for (int i=0; i<= size; i++){
+            hangman[i] = positions[i];
+        }
 
+    }
+    std::string getPositions(int pos){
+        return hangman[pos];
+    }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Example of Word from sports SubClass: Potential to add file with word list example;
     Word:Sports footBall;
     footBall.setWord("Penalty");
     footBall.setHint("Misconduct");
-    std::cout<<"Word: "<< footBall.getWord()<< "Hint: "<< footBall.getHint()<<std::endl;
+    std::cout<<"Word: "<< footBall.getWord()<<  "Hint: "<< footBall.getHint()<<std::endl;
 
+    // Example of Player initial lives with overloaded constructor
+    Player playerOne;
+    std::cout<<playerOne.getLives()<<std::endl;
+    std::string positions[]{
+            " +---+\n"
+            " |   |\n"
+            "     |\n"
+            "     |\n"
+            "     |\n"
+            "     |\n"
+            "=========\n",
+            "+---+\n"
+            "|   |\n"
+            "0   |\n"
+            "|   |\n"
+            "    |\n"
+            "    |\n"
+            "=========\n",
+            " +---+\n"
+            " |   |\n"
+            " O   |\n"
+            "/|   |\n"
+            "     |\n"
+            "     |\n",
+            "=========\n"
+            " +---+\n"
+            " |   |\n"
+            " O   |\n"
+            "/|\\  |\n"
+            "     |\n"
+            "     |\n"
+            "=========\n",
+            " +---+\n"
+            " |   |\n"
+            " O   |\n"
+            "/|\\  |\n"
+            "/    |\n"
+            "     |\n"
+            "=========\n",
+            " +---+\n"
+            " |   |\n"
+            " O   |\n"
+            "/|\\  |\n"
+            "/ \\  |\n"
+            "     |\n"
+            "=========\n"
+    };
+    ScoreBoard uno;
+    uno.setPositions(positions,6);
 
+    for(int i=0; i<=5; i++){
+        std::cout<<uno.getPositions(i);
+    }
     return 0;
 }

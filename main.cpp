33d894336b883ea435bc6aa5436f934f91e2058c
toj900 <46387607+toj900@users.noreply.github.com>
2020-12-03 18:37:24 -0500

    Player Input
    Added basic play, can tell if a guess is correct or incorrect.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 
 class Word{
 private:
@@ -57,7 +57,7 @@ private:
     std::string hangman[7];
 public:
     void setPositions(std::string *positions, int size){
-        for (int i=0; i<= size; i++){
+        for (int i=0; i<= size-1; i++){
             hangman[i] = positions[i];
         }
 
@@ -68,6 +68,8 @@ public:
 };
 
 int main() {
+    // Todo: add while loop to guess words.
+
     // Example of Word from sports SubClass: Potential to add file with word list example;
     Word:Sports footBall;
     footBall.setWord("Penalty");
@@ -76,6 +78,8 @@ int main() {
 
     // Example of Player initial lives with overloaded constructor
     Player playerOne;
+
+    // Hangman print screen
     std::cout<<playerOne.getLives()<<std::endl;
     std::string positions[]{
             " +---+\n"
@@ -84,6 +88,13 @@ int main() {
             "     |\n"
             "     |\n"
             "     |\n"
+            "=========\n",
+            "+---+\n"
+            "|   |\n"
+            "0   |\n"
+            "    |\n"
+            "    |\n"
+            "    |\n"
             "=========\n",
             "+---+\n"
             "|   |\n"
@@ -97,8 +108,8 @@ int main() {
             " O   |\n"
             "/|   |\n"
             "     |\n"
-            "     |\n",
-            "=========\n"
+            "     |\n"
+            "=========\n",
             " +---+\n"
             " |   |\n"
             " O   |\n"
@@ -121,11 +132,46 @@ int main() {
             "     |\n"
             "=========\n"
     };
+    //Initializing  scoreboard class with ascii art
     ScoreBoard uno;
-    uno.setPositions(positions,6);
+    uno.setPositions(positions,7);
 
-    for(int i=0; i<=5; i++){
-        std::cout<<uno.getPositions(i);
+    // Test print of ascii art
+    for(int i=0; i<=6; i++){
+        //std::cout<<uno.getPositions(6);
     }
+
+    // Todo: Replace the following code with class calls.
+    std::string word = "ass";
+    std::string guess;
+    std::cout<<"Enter a letter: ";
+    std::cin>>(guess);
+    //std::string guess = "s";
+
+    int size =word.size();
+    bool reduce = true;
+
+    std::vector<char> l (3, '*');
+    // Loop to compare word and player  guess, If the guess is correct will replace each * with the corresponding letters
+    // If the guess is wrong, it will change the reduce bool to later reduce total lives
+    for (int i=0; i<=size-1;i++){
+        if (word[i] == guess[0]){
+            l[i] = guess[0];
+            reduce = false;
+
+        }
+        std::cout<<l[i];
+    }
+
+    if (reduce) {
+        playerOne.setLives(1);
+    }
+    std::cout<<"\n"<<(6-playerOne.getLives());
+
+    std::cout<<"\nWord: ";
+    for (int i=0; i<=size-1;i++){
+            std::cout<<l[i];
+        }
+    std::cout<<"\n"<<uno.getPositions(6-playerOne.getLives());
     return 0;
 }

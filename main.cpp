ed3dab6dbf8ecbc00d392a5d6c30d43bd492181a
toj900 <46387607+toj900@users.noreply.github.com>
2020-12-03 15:42:40 -0500

    Class Implementation
    Added basic classes to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
 
+class Word{
+private:
+    std::string word;
+    std::string fileName;
+
+public:
+    void setWord(  std::string newWord){
+        word = newWord;
+    }
+    std::string getWord(){
+        return word;
+    }
+    void setFileName(  std::string newFile){
+        fileName = newFile;
+    }
+    std::string getFile(){
+        return fileName;
+    }
+};
+
+class Sports: public Word{
+public:
+};
+class Music: public Word{
+
+};
+class History: public Word{
+
+};
+class Player{
+private:
+    int lives =6;
+
+public:
+    void setLives(int reduction){
+        lives -= reduction;
+    }
+    int getLives(){
+        return lives;
+    }
+};
+
+class ScoreBoard{
+
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     int i = 0;

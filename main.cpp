44c9d2380d6491a0cb7951c4127b6192fd4ad400
toj900 <46387607+toj900@users.noreply.github.com>
2020-12-05 12:28:02 -0500

    Working Version
    //Need to implement class structure
    //Word dictionary
    //Clean up output

--- main.cpp
+++ main.cpp
@@ -2,77 +2,108 @@
 #include <vector>
 #include <algorithm>
 
+<<<<<<< HEAD
+class Word {
+=======
+
 class Word{
+
 private:
     std::string word;
     std::string fileName;
     std::string hint;
 public:
-    void setWord(  std::string newWord){
+    void setWord(std::string newWord) {
         word = newWord;
     }
+
+<<<<<<< HEAD
+    std::string getWord() {
+=======
     std::string getWord(){
+
         return word;
     }
-    void setFileName(  std::string newFile){
+
+    void setFileName(std::string newFile) {
         fileName = newFile;
     }
-    std::string getFile(){
+
+<<<<<<< HEAD
+    std::string getFile() {
         return fileName;
     }
+
+    void setHint(std::string newHint) {
+=======
     void setHint(  std::string newHint){
+
         hint = newHint;
     }
-    std::string getHint(){
+
+    std::string getHint() {
         return hint;
     }
 };
 
-class Sports: public Word{
+class Sports : public Word {
 public:
+<<<<<<< HEAD
 };
+
+class Music : public Word {
+=======
+
 class Music: public Word{
 
-};
-class History: public Word{
 
 };
-class Player{
+
+class History : public Word {
+
+};
+
+class Player {
 private:
     int lives;
 
 public:
-    Player(){
-        lives =6;
+    Player() {
+        lives = 6;
     }
-    void setLives(int reduction){
+
+    void setLives(int reduction) {
         lives -= reduction;
     }
-    int getLives(){
+
+    int getLives() {
         return lives;
     }
 };
 
-class ScoreBoard{
+class ScoreBoard {
 private:
     std::string hangman[7];
 public:
-    void setPositions(std::string *positions, int size){
-        for (int i=0; i<= size-1; i++){
+    void setPositions(std::string *positions, int size) {
+        for (int i = 0; i <= size - 1; i++) {
             hangman[i] = positions[i];
         }
 
     }
-    std::string getPositions(int pos){
+
+    std::string getPositions(int pos) {
         return hangman[pos];
     }
 };
+
 
 int main() {
     // Todo: add while loop to guess words.
 
     // Example of Word from sports SubClass: Potential to add file with word list example;
-    Word:Sports footBall;
+    Word:
+    Sports footBall;
     footBall.setWord("Penalty");
     footBall.setHint("Misconduct");
     //std::cout<<"Word: "<< footBall.getWord()<<  "Hint: "<< footBall.getHint()<<std::endl;
@@ -134,57 +165,70 @@ int main() {
     };
     //Initializing  scoreboard class with ascii art
     ScoreBoard uno;
-    uno.setPositions(positions,7);
+    uno.setPositions(positions, 7);
 
     // Test print of ascii art
-    for(int i=0; i<=6; i++){
+    for (int i = 0; i <= 6; i++) {
         //std::cout<<uno.getPositions(6);
     }
 
     // Todo: Replace the following code with class calls.
-    std::string word = "ass";
+    std::string word = "astronomy";
     std::string guess;
 
 
+    int size = word.size();
 
-    int size =word.size();
-    bool reduce = true;
-
+    int count = 0;
     // Use of STL Container in the form of a vector
-    std::vector<char> l (size, '*');
+    std::vector<char> l(size, '*');
 
-    std::cout<<"\n"<<("Word: ");
+    std::cout << "\n" << ("Word: ");
     //Example use of STL algorithm(s) <for_each> //Which replaces the following for loop
-    auto print = [](const char& n) { std::cout << n; };
+    auto print = [](const char &n) { std::cout << n; };
     // Example of use of STL iterator(s) //<vector>.cbegin and <vector>.cend
-    std::for_each(l.cbegin(), l.cend(), print);
+    while (playerOne.getLives() != 0) {
+        bool reduce = true;
 
-    std::cout<<"\nEnter a letter: ";
-    std::cin>>(guess);
-    // Loop to compare word and player  guess, If the guess is correct will replace each * with the corresponding letters
-    // If the guess is wrong, it will change the reduce bool to later reduce total lives
-    for (int i=0; i<=size-1;i++){
-        if (word[i] == guess[0]){
-            l[i] = guess[0];
-            reduce = false;
-
+        for (int i = 0; i <= size - 1; i++) {
+            if (word[i] == guess[0]) {
+                l[i] = guess[0];
+                ++count;
+                std::cout << count << "/" << size << std::endl;
+            }
         }
-        //std::cout<<l[i];
-    }
-    std::for_each(l.cbegin(), l.cend(), print);
-    if (reduce) {
-        playerOne.setLives(1);
-    }
-
-
-
-    /*
-    std::cout<<"\nWord: ";
-    for (int i=0; i<=size-1;i++){
-            std::cout<<l[i];
+        if (count == size - 1) {
+            playerOne.setLives(6);
         }
-    */
+        std::for_each(l.cbegin(), l.cend(), print);
 
-    std::cout<<"\n"<<uno.getPositions(6-playerOne.getLives());
+        std::cout << "\nEnter a letter: ";
+        std::cin >> (guess);
+        // Loop to compare word and player  guess, If the guess is correct will replace each * with the corresponding letters
+        // If the guess is wrong, it will change the reduce bool to later reduce total lives
+        for (int i = 0; i <= size - 1; i++) {
+            if (word[i] == guess[0]) {
+                l[i] = guess[0];
+                reduce = false;
+
+            }
+            //std::cout<<l[i];
+        }
+        std::for_each(l.cbegin(), l.cend(), print);
+        if (reduce) {
+            playerOne.setLives(1);
+        }
+
+
+
+        /*
+        std::cout<<"\nWord: ";
+        for (int i=0; i<=size-1;i++){
+                std::cout<<l[i];
+            }
+        */
+
+        std::cout << "\n" << uno.getPositions(6 - playerOne.getLives());
+    }
     return 0;
 }

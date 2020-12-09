acd66bcbdd63904517569225d1557826c2c33009
Aashwin <aashwinshankar@gmail.com>
2020-12-08 21:19:12 -0500

    Change string array in classes to vectors

--- main.cpp
+++ main.cpp
@@ -4,17 +4,18 @@
 #include <vector>
 
 
-class Word{
+class Word {
 private:
     std::string word;
     std::string fileName;
     std::string hint;
+
 public:
     void setWord(  std::string newWord) {
         word = newWord;
     }
 
-    std::string getWord() {
+    std::string getWord() const {
         return word;
     }
     void setFileName(  std::string newFile) {
@@ -27,29 +28,44 @@ public:
     std::string getHint() {
         return hint;
     }
+    virtual void addWord() = 0;
 };
 
 class Sports: public Word {
 private:
-    string sports = ["soccer", "basketball", "cricket", "tennis", "boxing", "hockey", "volleyball",
-                        "golf", "badminton", "baseball", "football", "rugby", "wrestling"];
+    std::vector<std::string> sports;
     //should this be "word" instead of "sports"? apply this to the other classes :)
+
 public:
-    
+    Sports() {
+        sports = {"soccer", "basketball", "cricket", "tennis", "boxing", "hockey", "volleyball",
+            "golf", "badminton", "baseball", "football", "rugby", "wrestling"};
+    }
+    void addWord() override {
+        std::string input;
+        std::cout << "Word to add to sports: ";
+        std::cin >> input;
+        sports.push_back(input);
+    }
 };
 class Music: public Word {
 private:
-    string music = ["jazz", "pop", "rap", "rock", "classical", "reggae", "hiphop", "blues"];
+    std::vector<std::string> music;
+
 public:
-    
+    Music() {
+        music = {"jazz", "pop", "rap", "rock", "classical", "reggae", "hiphop", "blues"};
+    }
 };
 //I changed this from history to food because im terrible at guessing history, change it if u want
 class Food: public Word {
 private:
-    string food = ["grape", "tomato", "cheese", "chocolate", "eggroll", "pizza", "ramen", "apple"];
-public:
+    std::vector<std::string> food;
 
-    
+public:
+    Food() {
+        food = {"grape", "tomato", "cheese", "chocolate", "eggroll", "pizza", "ramen", "apple"};
+    }
 };
 class Player{
 private:
@@ -57,7 +73,7 @@ private:
 
 public:
     Player() {
-        lives =6;
+        lives = 6;
     }
     void setLives(int reduction) {
         lives -= reduction;
@@ -72,7 +88,7 @@ private:
     std::string hangman[7];
 public:
     void setPositions(std::string *positions, int size) {
-        for (int i=0; i<= size-1; i++) {
+        for (int i = 0; i <= size - 1; i++) {
             hangman[i] = positions[i];
         }
 
@@ -87,7 +103,7 @@ int main() {
     // Todo: add while loop to guess words.
 
     // Example of Word from sports SubClass: Potential to add file with word list example;
-    Word:Sports footBall;
+    Sports footBall;
     footBall.setWord("Penalty");
     footBall.setHint("Misconduct");
     //std::cout<<"Word: "<< footBall.getWord()<<  "Hint: "<< footBall.getHint()<<std::endl;
@@ -95,6 +111,9 @@ int main() {
     // Example of Player initial lives with overloaded constructor
     Player playerOne;
 
+    // This should be moved to a class so image can be redrawn by calling a function
+    // I think specific lines can be overwritten https://codereview.stackexchange.com/a/186537/234023
+    // This way previous views are not seen
     // Hangman print screen
     std::string positions[] {
             " +---+\n"
@@ -162,7 +181,7 @@ int main() {
 
 
 
-    int size =word.size();
+    int size = word.size();
     bool reduce = true;
 
     // Use of STL Container in the form of a vector
@@ -178,7 +197,7 @@ int main() {
     std::cin >> (guess);
     // Loop to compare word and player  guess, If the guess is correct will replace each * with the corresponding letters
     // If the guess is wrong, it will change the reduce bool to later reduce total lives
-    for (int i = 0; i <= size-1; i++){
+    for (int i = 0; i <= size - 1; i++){
         if (word[i] == guess[0]){
             l[i] = guess[0];
             reduce = false;
@@ -191,8 +210,6 @@ int main() {
         playerOne.setLives(1);
     }
 
-
-
     /*
     std::cout<<"\nWord: ";
     for (int i=0; i<=size-1;i++){
@@ -200,6 +217,6 @@ int main() {
         }
     */
 
-    std::cout << "\n" << uno.getPositions(6-playerOne.getLives());
+    std::cout << "\n" << uno.getPositions(6 - playerOne.getLives());
     return 0;
 }
